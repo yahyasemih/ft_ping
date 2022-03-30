359cda24d2ac566db1df6bd74b100547d6f13a02
Yahya EZ-ZAINABI <yez-zain@student.1337.ma>
2022-03-30 23:15:17 +0000

    Fix formatting

--- utilities.c
+++ utilities.c
@@ -31,28 +31,23 @@ int dns_resolve(const char *host, char *dest, int size) {
 }
 
 unsigned short ft_checksum(unsigned short *addr, int len) {
-    int nleft = len;
-    int sum = 0;
-    unsigned short *w = addr;
-    unsigned short answer = 0;
+	int nleft = len;
+	int sum = 0;
+	unsigned short *w = addr;
+	unsigned short answer = 0;
 
-    while (nleft > 1)
-    {
-      sum += *w++;
-      nleft -= 2;
-    }
-
-    if (nleft == 1)
-    {
-      *(unsigned char *)(&answer) = *(unsigned char *)w;
-      sum += answer;
-    }
-
-    sum = (sum >> 16) + (sum & 0xffff);
-    sum += (sum >> 16);
-    answer = ~sum;
-
-    return (answer);
+	while (nleft > 1) {
+		sum += *w++;
+		nleft -= 2;
+	}
+	if (nleft == 1) {
+		*(unsigned char *)(&answer) = *(unsigned char *)w;
+		sum += answer;
+	}
+	sum = (sum >> 16) + (sum & 0xffff);
+	sum += (sum >> 16);
+	answer = ~sum;
+	return (answer);
 }
 
 int search_char(const char *s, char c) {
@@ -84,7 +79,6 @@ int is_fqdn(const char *host) {
 			nb_dots++;
 		}
 	}
-
 	return nb_dots > 0;
 }
 
